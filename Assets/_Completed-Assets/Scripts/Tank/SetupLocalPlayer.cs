60780ee8ec8519b9281f785b873690e34e0e1aea
jennachoo <seohyunchoo@gmail.com>
2017-01-16 16:47:29 -0500

    First commit

---
+++ Assets/_Completed-Assets/Scripts/Tank/SetupLocalPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer)
+        {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Completed-Assets/Scripts/Tank/TankCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+namespace Complete
+{
+    public class TankCamera : NetworkBehaviour
+    {
+        public int m_PlayerNumber = 1;              // Used to identify the different players..
+        public GameObject CameraRig;
+
+        private void OnEnable()
+        {
+
+
+        }
+
+
+        private void Start ()
+        {
+            if (!isLocalPlayer)
+            {
+                CameraRig.SetActive(false);
+            }
+        }
+    }
+}
