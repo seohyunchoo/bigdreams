d662e993c00f02b185a1e184089d28175a57577d
maverickchen <mzc@andrew.cmu.edu>
2017-01-16 18:24:10 -0500

    maverick figured out cameras

--- Assets/_Completed-Assets/Scripts/Tank/SetupLocalPlayer.cs
+++ Assets/_Completed-Assets/Scripts/Tank/SetupLocalPlayer.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class SetupLocalPlayer : NetworkBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		if (isLocalPlayer)
-        {
-
+        {   
+            GetComponent<Complete.TankHealth>().enabled = true;
+            GetComponent <Complete.TankMovement> ().enabled = true;
+            GetComponent <Complete.TankShooting> ().enabled = true;
+            GetComponentInChildren<Camera>().enabled = true;
+            Camera.main.transform.position = this.transform.position - this.transform.forward * 2 + this.transform.up * 3;
+            Camera.main.transform.LookAt(this.transform.position);
+            Camera.main.transform.parent = this.transform;
         }
 	}
 	
